c213a307a5e8257c829fc81d85db4b4608a922ef
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2016-01-28 00:22:46 +0300

    (INIT): Extracted Toml-related features as a separate plugin         out IntelliJ Rust one

---
+++ src/test/kotlin/org/toml/lang/core/parser/TomlCompleteParsingTestCase.kt
@@ -0,0 +1,10 @@
+package org.toml.lang.core.parser
+
+class TomlCompleteParsingTestCase : TomlParsingTestCaseBase("well-formed") {
+
+    // @formatter:off
+
+    public fun testExample()               { doTest(true) }
+
+    // @formatter:on
+}
